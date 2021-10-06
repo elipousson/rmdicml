ef315a59d5dc5bccd38a6736f8b7f718be36446c
Eli Pousson <eli.pousson@gmail.com>
2021-10-06 17:11:54 -0400

    feat: Initial commit w/ simple_icml_format function

---
+++ R/simple_icml_format.R
@@ -0,0 +1,11 @@
+#' IMCL Export format for RMarkdown
+#'
+#' @export
+#' @importFrom rmarkdown output_format knitr_options pandoc_options
+simple_icml_format = function() {
+  output_format(
+    knitr = knitr_options(opts_chunk = list(dev = 'png')),
+    pandoc = pandoc_options(to = "icml"),
+    clean_supporting = FALSE
+  )
+}
