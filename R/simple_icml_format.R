0856764ca24278999aa4380ae6cc7545af6f5929
Eli Pousson <eli.pousson@gmail.com>
2021-10-08 21:58:52 -0400

    Update simple_icml_format.R

--- R/simple_icml_format.R
+++ R/simple_icml_format.R
@@ -3,11 +3,15 @@
 #' @export
 #' @importFrom rmarkdown output_format knitr_options pandoc_options
 simple_icml_format = function() {
+
+  args <- c("--self-contained")
+  args <- c(args, "--number-sections")
+
   output_format(
     knitr = knitr_options(opts_chunk = list(dev = 'png')),
     pandoc = pandoc_options(
       to = "icml",
-      args = "-s"),
+      args = args),
     clean_supporting = FALSE
   )
 }
