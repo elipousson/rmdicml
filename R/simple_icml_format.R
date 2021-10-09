60c108b890f35945a82e77589d9f5456bc6954cb
Eli Pousson <eli.pousson@gmail.com>
2021-10-08 21:09:36 -0400

    Update simple_icml_format.R

--- R/simple_icml_format.R
+++ R/simple_icml_format.R
@@ -6,7 +6,7 @@ simple_icml_format = function() {
   output_format(
     knitr = knitr_options(opts_chunk = list(dev = 'png')),
     pandoc = pandoc_options(to = "icml"),
-    args = c(-s),
+    args = c("-s"),
     clean_supporting = FALSE
   )
 }
